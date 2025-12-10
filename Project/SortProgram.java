60ca3b03b8010251b7d5fb3c165721bdc140272f
porametpu <poramet.pu@kkumail.com>
2025-12-10 20:25:13 +0700

    Add BubbleSort

--- Project/SortProgram.java
+++ Project/SortProgram.java
@@ -62,6 +62,18 @@ public class SortProgram {
         return i + 1;
     }
 
-
-
+//BubbleSort
+public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+    }
+ }
 }
+
